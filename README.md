2670652594dd0c4ee405802a5b60b6392caa2265
Michael Bittencourt <mabm@cesar.org.br>
2022-05-11 09:45:19 -0300

    feature: Add script to download and install dotfiles

---
+++ README.md
@@ -0,0 +1,16 @@
+# Michael Bittencourt Dotfiles
+
+This project was created to make easy install dotfiles in an other environment
+
+## Install
+
+### With curl
+
+```bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/MichaelBittencourt/.dotfiles/main/donwload.sh)"
+```
+
+### With wget
+```bash
+bash -c "$(wget https://raw.githubusercontent.com/MichaelBittencourt/.dotfiles/main/donwload.sh -O -)"
+```
---
+++ donwload.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+DOTFILES_LINK="https://github.com/MichaelBittencourt/.dotfiles.git"
+DOTFILES_PATH="$HOME/.dotfiles"
+
+function clone_dotfiles() {
+    create_backup "$DOTFILES_PATH"
+    git clone --depth=1 "$DOTFILES_LINK" "$DOTFILES_PATH"
+}
+
+function installDotFiles() {
+    cd "$DOTFILES_PATH"
+    bash "install.sh" || return 3
+}
+
+function create_backup() {
+    local file_or_folder="$1"
+    if [ -n "${file_or_folder}" ]; then
+        if [ -h "${file_or_folder}" ]; then
+            unlink ${file_or_folder}
+        elif [ -f "${file_or_folder}" ] || [ -d "${file_or_folder}" ]; then
+            echo "Creating a backup to $file_or_folder in ${file_or_folder}.bkp"
+            mv $file_or_folder ${file_or_folder}.bkp
+        fi
+    else
+        echo "create_backup need a param"
+    fi
+}
+
+function main() {
+    clone_dotfiles || echo "Error to .dontfiles!"
+    installDotFiles || echo "Error to install dotfiles"
+}
+
+main
